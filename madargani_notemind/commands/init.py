54b9f5068b46fa2177a3f194da2fb2f4cfe8e3d6
madargani <mad3913@gmail.com>
2025-07-11 17:11:02 -0700

    check if notemind directory exists

--- madargani_notemind/commands/init.py
+++ madargani_notemind/commands/init.py
@@ -1,7 +1,23 @@
 import typer
 
+from pathlib import Path
+from typing import Optional
+from typing_extensions import Annotated
+
 app = typer.Typer()
 
 @app.command()
-def init():
-    print('Initializing NoteMind directory')
+def init(
+    base_dir: Annotated[Path, typer.Argument()] = Path.cwd()
+):
+    """
+    Create hidden notemind directory if it doesn't exist
+    """
+
+    print("base dir: ", base_dir.absolute())
+
+    # Check if .notemind exists
+    if (base_dir / '.notemind').exists():
+        print('notemind directory already exists')
+
+    # Create .notemind directory
