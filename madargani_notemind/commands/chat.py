5fb31458a85e5695d031f0dcfcc51556c3b85713
madargani <mad3913@gmail.com>
2025-07-21 18:20:17 -0700

    starting with ollama

--- madargani_notemind/commands/chat.py
+++ madargani_notemind/commands/chat.py
@@ -2,16 +2,17 @@ import typer
 import chromadb
 from pprint import pprint
 from pathlib import Path
+import ollama
 
 app = typer.Typer()
 
 @app.command()
 def chat(query: str):
-    base_dir = Path.cwd()
-
-    client = chromadb.PersistentClient(base_dir / '.notemind/vector_db.chroma')
-    collection = client.get_collection('notemind')
-
-    results = collection.query(query_texts=query, n_results=3)
-    pprint(results['documents'])
-
+    response = ollama.chat(
+        model='gemma3n',
+        messages=[{
+            'role': 'user',
+            'content': query
+        }]
+    )
+    print(response)
